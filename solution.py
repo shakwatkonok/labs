2a1f9d544b2ef68001f557d9e711b87bb363f0a7
shakwatkonok <78381390+shakwatkonok@users.noreply.github.com>
2021-02-25 02:52:07 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -63,7 +63,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Message ends with a single period.
     # Fill in start
     clientSocket.send(endmsg.encode())
-    recv5 = clientSocket.recv(1024).decode()
+    # recv5 = clientSocket.recv(1024).decode()
     # print(recv5)
     # Fill in end
 
