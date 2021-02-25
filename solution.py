fbddf8170be6007bcc6fb6a76359f4d4e17b2014
shakwatkonok <78381390+shakwatkonok@users.noreply.github.com>
2021-02-25 00:18:57 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -15,7 +15,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    print(recv)
+    # print(recv)
     if recv[:3] != '220':
         print('220 reply not received from server.')
 
@@ -23,7 +23,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     heloCommand = 'HELO Alice\r\n'
     clientSocket.sendall(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print(recv1)
+    # print(recv1)
     if recv1[:3] != '250':
         print('250 reply not received from server.')
 
@@ -32,7 +32,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     mailfromCommand = 'MAIL FROM: <example@yahoo.com>\r\n'
     clientSocket.sendall(mailfromCommand.encode())
     recv2 = clientSocket.recv(1024).decode()
-    print(recv2)
+    # print(recv2)
 
 
     # Fill in end
@@ -42,7 +42,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     rcpttoCommand = 'RCPT TO: <konok93@gmail.com>\r\n'
     clientSocket.sendall(rcpttoCommand.encode())
     recv3 = clientSocket.recv(1024).decode()
-    print(recv3)
+    # print(recv3)
 
     # Fill in end
 
@@ -51,7 +51,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     dataCommand = 'Data\r\n'
     clientSocket.sendall(dataCommand.encode())
     recv4 = clientSocket.recv(1024).decode()
-    print(recv4)
+    # print(recv4)
 
     # Fill in end
 
@@ -64,7 +64,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     clientSocket.sendall(endmsg.encode())
     recv5 = clientSocket.recv(1024).decode()
-    print(recv5)
+    # print(recv5)
     # Fill in end
 
     # Send QUIT command and get server response.
@@ -73,7 +73,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     print(quitCommand)
     clientSocket.send(quitCommand.encode())
     recv6 = clientSocket.recv(1024).decode()
-    ptint(recv6)
+    # ptint(recv6)
     # Fill in end
 
 
